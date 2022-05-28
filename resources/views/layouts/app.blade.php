2b19d677a1141578f952916de0988d3621b71421
damijan0034 <damijan0034@gmail.com>
2022-05-28 16:30:05 +0200

    third commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ route('dashboard.index') }}">
-                   <img width="100" height="50" src="{{ url('/storage/logo.jpg') }}" alt="">
+                   <img width="100" height="50" src="{{ url('/images/logo.jpg') }}" alt="">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
